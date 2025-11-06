fae5b1dc71a174c9a93c6c098b39123e388e9d05
[Joy Swarnakar] <[joyswarnakar3@gmail.com]>
2025-11-06 12:02:55 +0530

    first push to production

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState,useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState("")
+
+  useEffect(()=> {
+    fetch("http://localhost:4000/api/message")
+    .then((res)=> res.json())
+    .then((data) => setMessage(data.message))
+    .catch((err) => {
+      console.error("error fetching message", err);
+      
+    })
+  }, [])
+
+  return (
+    <>
+    <h1>Welcome to joy frontend </h1>
+    <h2>data {message}</h2>
+    </>
+  )
+}
+
+export default App
